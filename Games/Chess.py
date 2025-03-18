27483121b638cc58af0a786e93e4b0e99ff409f4
licnex <mominravage@gmail.com>
2025-03-18 19:13:52 +0500

    Made chess better

--- Games/Chess.py
+++ Games/Chess.py
@@ -1,235 +1,101 @@
-"""Edited version of https://github.com/BiancaNCoelho/ChessCLI"""
 import curses
-import re
+import chess
 
-inputTranslator = {
-    "1": 7,
-    "2": 6,
-    "3": 5,
-    "4": 4,
-    "5": 3,
-    "6": 2,
-    "7": 1,
-    "8": 0,
-    "a": 0,
-    "b": 1,
-    "c": 2,
-    "d": 3,
-    "e": 4,
-    "f": 5,
-    "g": 6,
-    "h": 7,
-}
-
-class Chess:
-    def __init__(self, whiteName, blackName, turnRepository):
-        self.sideSize = 8
-        self.table = Table()
-        self.blackPlayer = Player(blackName, turnRepository)
-        self.whitePlayer = Player(whiteName, turnRepository)
-
-    def playTurn(self, userInput):
-        regex = re.compile("^[a-h][1-8] [a-h][1-8]$")
-        if regex.match(userInput) is not None:
-            fromPos = Position(inputTranslator[userInput[1]], inputTranslator[userInput[0]])
-            toPos = Position(inputTranslator[userInput[4]], inputTranslator[userInput[3]])
-            if self.table.movePiece(fromPos, toPos):
-                return True
-        return False
-
-    def __repr__(self):
-        return self.table.__repr__()
-
-class Player:
-    def __init__(self, name, turnRepository):
-        self.name = name
-        self.turnRepository = turnRepository
-
-    def play(self, chess, fromPosition, toPosition):
-        if not self._canPlay():
-            return
-        print("playing")
-
-    def _canPlay(self):
-        return self.turnRepository.getCurrent() == self.name
-
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class TurnRepository:
-    def __init__(self, whiteName, blackName):
-        self.turns = [(whiteName, "WHITE"), (blackName, "BLACK")]
-        self.winner = None
-
-    def getCurrent(self):
-        return self.turns[0][0]
-
-    def getCurrentColor(self):
-        return self.turns[0][1]
-    
-    def nextTurn(self):
-        self.turns.reverse()
-
-    def finished(self):
-        return self.winner is not None
-
-class Table:
-    def __init__(self):
-        self.size = 8
-        self.table = [[" "] * self.size for _ in range(self.size)]
-        self._initPieces()
-
-    def __repr__(self):
-        repr_str = ""
-        repr_str += "     A     B     C     D     E     F     G     H\n"
-        repr_str += "    _____________________________________________\n"
-        aux = ["8", "7", "6", "5", "4", "3", "2", "1"]
-        for i in range(self.size):
-            repr_str += aux[i] + " | " + " | ".join(self.table[i][j].__repr__() for j in range(self.size)) + " |\n"
-            repr_str += "    _____________________________________________\n"
-        return repr_str
-        
-    def _initPieces(self):
-        for i in range(self.size):
-            self.table[1][i] = Pawn("W")
-            self.table[6][i] = Pawn("B")
-
-        self.table[0][0] = Rook("W")
-        self.table[7][0] = Rook("B")
-
-        self.table[0][1] = Knight("W")
-        self.table[7][1] = Knight("B")
-
-        self.table[0][2] = Bishop("W")
-        self.table[7][2] = Bishop("B")
-
-        self.table[0][3] = Queen("W")
-        self.table[7][3] = Queen("B")
-
-        self.table[0][4] = King("W")
-        self.table[7][4] = King("B")
-
-        self.table[0][5] = Bishop("W")
-        self.table[7][5] = Bishop("B")
-
-        self.table[0][6] = Knight("W")
-        self.table[7][6] = Knight("B")
-        
-        self.table[0][7] = Rook("W")
-        self.table[7][7] = Rook("B")
-
-    def movePiece(self, fromPos, toPos):
-        piece = self.table[fromPos.x][fromPos.y]
-        if piece == " ":
-            return False
-        intermediatePieces = self.getIntermediatepiecesFromTo(fromPos, toPos)
-        if not piece.canMoveTo(fromPos, toPos, intermediatePieces):
-            return False
-        secondPiece = self.table[toPos.x][toPos.y]
-        if secondPiece != " ":
-            # Handle capturing the piece
-            pass
-        self.table[fromPos.x][fromPos.y] = " "
-        self.table[toPos.x][toPos.y] = piece
-        return True
-
-    def getIntermediatepiecesFromTo(self, fromPos, toPos):
-        return []
-
-class Pawn:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "P-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-class Bishop:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "B-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-class Knight:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "N-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-class Rook:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "R-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-class Queen:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "Q-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-class King:
-    def __init__(self, colorId):
-        self.colorId = colorId
-    def __repr__(self):
-        return "K-" + self.colorId
-    def canMoveTo(self, fromPos, toPos, intermediatePieces):
-        return True
-
-def draw_board(stdscr, chess, turnRepository):
-    stdscr.clear()
-    try:
-        height, width = stdscr.getmaxyx()
-        if height < 20 or width < 40:
-            raise ValueError("Window too small")
-        stdscr.addstr(0, 0, f"It's the turn of {turnRepository.getCurrent()} - {turnRepository.getCurrentColor()}\n")
-        stdscr.addstr(1, 0, str(chess))
-    except ValueError as e:
-        stdscr.clear()
-        stdscr.addstr(0, 0, str(e))
-        stdscr.addstr(1, 0, "Please resize the window and rerun the main.py")
-    stdscr.refresh()
-
-def get_input(stdscr, prompt):
-    stdscr.addstr(prompt)
-    stdscr.refresh()
-    curses.echo()
-    input_str = stdscr.getstr().decode('utf-8')
+def init_curses():
+    """Initialize curses settings."""
+    curses.initscr()
+    curses.start_color()
     curses.noecho()
-    return input_str
+    curses.cbreak()
+    curses.curs_set(0)  # Hide cursor
+    win = curses.newwin(20, 40, 0, 0)
+    win.keypad(True)
 
-def game(stdscr):
-    curses.curs_set(0)
-    stdscr.clear()
-    
-    stdscr.addstr("Welcome to Chess\n")
-    whiteName = get_input(stdscr, "Please enter the name of the player who will be white: ")
-    blackName = get_input(stdscr, "Please enter the name of the player who will be black: ")
+    # Define colors
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)  # White pieces
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)  # Black pieces
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK) # Highlighted selection
+    curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Cursor position
+    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)    # King in check
 
-    turnRepository = TurnRepository(whiteName, blackName)
-    chess = Chess(whiteName, blackName, turnRepository)
-    while not turnRepository.finished():
-        draw_board(stdscr, chess, turnRepository)
-        played = False
-        while not played:
-            userInput = get_input(stdscr, f"{turnRepository.getCurrent()} > ")
-            if not chess.playTurn(userInput):
-                height, width = stdscr.getmaxyx()
-                stdscr.addstr(height - 1, 0, "Illegal move. Please enter a move in the format 'e2 e4'.")
-                stdscr.refresh()
+    return win
+
+def draw_board(win, board, cursor_pos, selected_square, message=""):
+    """Render the chess board in curses with color."""
+    win.clear()
+    king_square = None
+
+    if board.is_check():
+        king_square = board.king(board.turn)  # Get king's position
+
+    for rank in range(8):
+        for file in range(8):
+            square = chess.square(file, 7 - rank)
+            piece = board.piece_at(square)
+            char = piece.symbol() if piece else "."
+
+            # Choose default color
+            color = curses.color_pair(1) if piece and piece.color == chess.WHITE else curses.color_pair(2)
+
+            # Highlight king if in check
+            if square == king_square:
+                color = curses.color_pair(5) | curses.A_BOLD
+
+            # Highlight selected piece
+            if selected_square == square:
+                win.addstr(rank + 1, file * 4 + 2, char, curses.color_pair(3) | curses.A_BOLD)
+            # Highlight cursor position
+            elif (file, rank) == cursor_pos:
+                win.addstr(rank + 1, file * 4 + 2, char, curses.color_pair(4) | curses.A_BOLD)
             else:
-                played = True
-        turnRepository.nextTurn()
+                win.addstr(rank + 1, file * 4 + 2, char, color)
+
+    win.addstr(10, 2, "Use arrow keys to move, Enter to select/move", curses.A_DIM)
+    win.addstr(12, 2, message, curses.A_BOLD if message else curses.A_DIM)
+    win.refresh()
+
+def main(win):
+    board = chess.Board()
+    cursor_pos = (0, 7)  # Start cursor at a8
+    selected_square = None
+    message = ""
+
+    draw_board(win, board, cursor_pos, selected_square, message)
+
+    while not board.is_game_over():
+        key = win.getch()
+
+        if key == curses.KEY_UP:
+            cursor_pos = (cursor_pos[0], max(0, cursor_pos[1] - 1))
+        elif key == curses.KEY_DOWN:
+            cursor_pos = (cursor_pos[0], min(7, cursor_pos[1] + 1))
+        elif key == curses.KEY_LEFT:
+            cursor_pos = (max(0, cursor_pos[0] - 1), cursor_pos[1])
+        elif key == curses.KEY_RIGHT:
+            cursor_pos = (min(7, cursor_pos[0] + 1), cursor_pos[1])
+        elif key == 10:  # Enter key
+            square = chess.square(cursor_pos[0], 7 - cursor_pos[1])
+
+            if selected_square is None:
+                if board.piece_at(square) and (board.turn == board.piece_at(square).color):
+                    selected_square = square  # Select piece
+                    message = "Piece selected!"
+                else:
+                    message = "Invalid selection!"
+            else:
+                move = chess.Move(selected_square, square)
+                if move in board.legal_moves:
+                    board.push(move)
+                    message = "Move played!"
+                    selected_square = None  # Reset selection
+                else:
+                    message = "Illegal move! Try again."
+
+        draw_board(win, board, cursor_pos, selected_square, message)
+
+    win.addstr(14, 10, "Game Over!", curses.A_BOLD)
+    win.getch()
 
 def play():
-    curses.wrapper(game)
+    curses.wrapper(main)
+play()
--- ReadMe.md
+++ ReadMe.md
@@ -42,7 +42,6 @@
       </ul>
     </li>
     <li><a href="#usage">Usage</a></li>
-    <li><a href="#roadmap">Roadmap</a></li>
     <li><a href="#contributing">Contributing</a></li>
     <li><a href="#license">License</a></li>
     <li><a href="#contact">Contact</a></li>
@@ -83,9 +82,18 @@ To get a local copy up and running follow these simple example steps.
 
 
 <!-- USAGE EXAMPLES -->
-## Usage
+## KeyNotes
+* If at any point you see an error make the window bigger it'll work then on if not open an issue at github.com/licnex/tgamehub
+* Most of these games use the arrow keys. 
+* If you are having a hard time understanding a game google it up bro.
+<details>
+  <summary>Chess</summary>
+  <p>
+    To play chess you use the arrow keys and enter for selection of a piece moreover there certain marks to indicate stuff eg a selected pawn would look like `[P]` and checked king would look like `k*`.If you are having trouble with the game saying "illegal selection/move" first watch this video https://www.youtube.com/watch?v=OCSbzArwB10 then if the problem presists open an issue
 
-- todo
+  </p>
+</details>
+
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 
@@ -138,7 +146,6 @@ Project Link: [https://github.com/Licnex/TGameHub](https://github.com/Licnex/TGa
 
 * Edited [https://github.com/amazingly-abhay/sudoku-cli/](https://github.com/amazingly-abhay/sudoku-cli/) for Sudoku
 * Edited [https://github.com/kevinmarquesp/snake_game](https://github.com/kevinmarquesp/snake_game) for Snake
-* Edited [https://github.com/BiancaNCoelho/ChessCLI](https://github.com/BiancaNCoelho/ChessCLI) for Chess
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
@@ -158,20 +165,3 @@ Project Link: [https://github.com/Licnex/TGameHub](https://github.com/Licnex/TGa
 [license-url]: https://github.com/Licnex/TGameHub/blob/master/LICENSE.txt
 [linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
 [linkedin-url]: https://linkedin.com/in/momin-khan-592797283
-[product-screenshot]: images/screenshot.png
-[Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
-[Next-url]: https://nextjs.org/
-[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
-[React-url]: https://reactjs.org/
-[Vue.js]: https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D
-[Vue-url]: https://vuejs.org/
-[Angular.io]: https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white
-[Angular-url]: https://angular.io/
-[Svelte.dev]: https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00
-[Svelte-url]: https://svelte.dev/
-[Laravel.com]: https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white
-[Laravel-url]: https://laravel.com
-[Bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
-[Bootstrap-url]: https://getbootstrap.com
-[JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
-[JQuery-url]: https://jquery.com 
